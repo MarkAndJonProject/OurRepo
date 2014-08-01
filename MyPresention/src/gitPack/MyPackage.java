0c33fbc41dfae0d6e9ad63c61eaa50662196d2e4
Mark A. Folashade <ayodeji.folashade@gmail.com>
2014-08-01 05:57:57 -0400

    Added Third Hello World and Edited Second Hello World

--- MyPresention/src/gitPack/MyPackage.java
+++ MyPresention/src/gitPack/MyPackage.java
@@ -3,6 +3,7 @@ package gitPack;
 public class MyPackage {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello World2");
+		System.out.println("Hello World 2");
+		System.out.println("Hello World 3");
 	}
 }
