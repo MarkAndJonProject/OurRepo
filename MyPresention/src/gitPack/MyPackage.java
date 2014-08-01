78fd4cfce27448e6741e0f38599b1be6d5eafba6
Mark Folashade <ayodeji.folashade@gmail.com>
2014-08-01 06:18:39 -0400

    Hello World to 1000!

--- MyPresention/src/gitPack/MyPackage.java
+++ MyPresention/src/gitPack/MyPackage.java
@@ -2,7 +2,7 @@ package gitPack;
 
 public class MyPackage {
 	public static void main(String[] args) {
-		for (int i = 1; i < 101; i++){
+		for (int i = 1; i < 1001; i++){
 			System.out.println("Hello World " + i);
 			
 		}
